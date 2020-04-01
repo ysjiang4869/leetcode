e36218ae7d5bed94fa739a0d2aa51119f286b24f
YueSong Jiang <1259012537@qq.com>
2020-04-01 17:58:10 +0800

    add 23 and 32

---
+++ src/main/java/org/jys/learn/leetcode/Lc0023MergeKSortedLists.java
@@ -0,0 +1,8 @@
+package org.jys.learn.leetcode;
+
+/**
+ * @author YueSong Jiang
+ * @date 2020/3/28
+ */
+public class Lc0023MergeKSortedLists {
+}
---
+++ src/main/java/org/jys/learn/leetcode/Lc0032LongestValidParentheses.java
@@ -0,0 +1,41 @@
+package org.jys.learn.leetcode;
+
+/**
+ * @author YueSong Jiang
+ * @date 2020/3/28
+ *
+ * @lc app=leetcode id=32 lang=java
+ *
+ * [32] Longest Valid Parentheses
+ *
+ * https://leetcode.com/problems/longest-valid-parentheses/description/
+ *
+ * algorithms
+ * Hard (27.37%)
+ * Likes:    2914
+ * Dislikes: 125
+ * Total Accepted:    256.9K
+ * Total Submissions: 934.8K
+ * Testcase Example:  '"(()"'
+ *
+ * Given a string containing just the characters '(' and ')', find the length
+ * of the longest valid (well-formed) parentheses substring.
+ *
+ * Example 1:
+ *
+ *
+ * Input: "(()"
+ * Output: 2
+ * Explanation: The longest valid parentheses substring is "()"
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: ")()())"
+ * Output: 4
+ * Explanation: The longest valid parentheses substring is "()()"
+ *
+ */
+public class Lc0032LongestValidParentheses {
+}
