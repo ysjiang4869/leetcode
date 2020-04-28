b70d1fae632fd9583ca55400d2394ced2b97c9ae
YueSong Jiang <1259012537@qq.com>
2020-04-28 20:42:29 +0800

    更新两个二分法题目

--- src/main/java/org/jys/learn/leetcode/Lc0033SearchInRotatedSortedArray.java
+++ src/main/java/org/jys/learn/leetcode/Lc0033SearchInRotatedSortedArray.java
@@ -45,7 +45,7 @@ package org.jys.learn.leetcode;
 public class Lc0033SearchInRotatedSortedArray {
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution3 solution = new Solution3();
         int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
         System.out.println(solution.search(nums, 0) == 4);
         System.out.println(solution.search(nums, 3) == -1);
@@ -64,6 +64,12 @@ public class Lc0033SearchInRotatedSortedArray {
         nums = new int[]{3, 1};
         System.out.println(solution.search(nums, 0) == -1);
         System.out.println(solution.search(nums, 3) == 0);
+
+        nums = new int[]{3, 5, 1};
+        System.out.println(solution.search(nums, 1) == 2);
+
+        nums = new int[]{5, 1, 3};
+        System.out.println(solution.search(nums, 1) == 1);
     }
 
     static class Solution {
@@ -110,6 +116,128 @@ public class Lc0033SearchInRotatedSortedArray {
                 }
                 mid = (hi + lo) / 2;
             }
+            return -1;
+
+        }
+    }
+
+    static class Solution2{
+        public int search(int[] nums, int target) {
+
+            if(nums.length==0){
+                return -1;
+            }
+
+            if(nums.length==1&& nums[0]!=target){
+                return -1;
+            }
+
+            //二分法查找分界点,
+            //分界点是第一个小于num[0]的值
+            //思路1是查找左边界，第一个小于num[0]的值
+            //思路2是查找右边界，即，最后一个递增的值，下一个就是转折点
+            //两种思路都行，关键是，在后面取lo和hi分半判断的时候，要注意lo=hi=mid的情况
+            int lo=0,hi=nums.length-1;
+            int mid=0;
+            while (lo<hi){
+                mid=lo+((hi-lo)>>1)+1;
+                if(nums[mid]>nums[0]){
+                    lo=mid;
+                }else {
+                    hi=mid-1;
+                }
+            }
+
+            //判断查找那一半
+            //lo/mid=第一个转折点，hi=lo/lo+1
+            //注意：查找左右边界，返回值应该是lo和hi，不是mid，这种情况可能mid!=lo？
+            if(target>=nums[0]){
+                //查找左边一半
+                hi=lo;//lo一定是最后一个递增，这个地方没错
+                lo=0;
+            }else {
+                //查找右边
+                hi=nums.length-1;
+                //lo一定是最后一个递增，要取下一个值
+                lo=Math.min(nums.length-1,lo+1);
+
+            }
+
+            while (lo<=hi){
+                mid=lo+((hi-lo)>>1);
+                if(nums[mid]==target){
+                    return mid;
+                }else if(nums[mid]>target){
+                   hi=mid-1;
+                }else {
+                    lo=mid+1;
+                }
+            }
+
+            return -1;
+
+        }
+    }
+
+    static class Solution3{
+        public int search(int[] nums, int target) {
+
+            if(nums.length==0){
+                return -1;
+            }
+
+            if(nums.length==1&& nums[0]!=target){
+                return -1;
+            }
+
+            //二分法查找分界点,
+            //分界点是第一个小于num[0]的值
+            //思路1是查找左边界，第一个小于num[0]的值
+            //思路2是查找右边界，即，最后一个递增的值，下一个就是转折点
+            //两种思路都行，关键是，在后面取lo和hi分半判断的时候，要注意lo=hi=mid的情况
+            //这里使用有边界
+            int lo=0,hi=nums.length-1;
+            int mid=0;
+            while (lo<hi){
+                mid=lo+((hi-lo)>>1);
+                if(nums[mid]<nums[0]){
+                    hi=mid;
+                }else {
+                    lo=mid+1;
+                }
+            }
+
+            //判断查找那一半
+            //lo/mid=第一个转折点，hi=lo/lo+1
+            if(target>=nums[0]){
+                //查找左边一半
+                //hi是第一个变小的，所以应该用hi-1
+                //但是存在全部递增，就没有hi，这时候hi应该是nums.length-1且该点不是拐点,就不需要减一了
+                if(nums.length-1==hi&&hi>0&&nums[hi]>nums[hi-1]){
+                    hi=hi;
+                }else {
+                    hi= Math.max(hi - 1, 0);//预防临界值
+                }
+                lo=0;
+
+            }else {
+                //查找右边
+                //mid一定是右半第一个元素
+                lo=hi;
+                hi=nums.length-1;
+            }
+
+            while (lo<=hi){
+                mid=lo+((hi-lo)>>1);
+                if(nums[mid]==target){
+                    return mid;
+                }else if(nums[mid]>target){
+                    hi=mid-1;
+                }else {
+                    lo=mid+1;
+                }
+            }
+
             return -1;
 
         }
--- src/main/java/org/jys/learn/leetcode/Lc0034FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/org/jys/learn/leetcode/Lc0034FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,8 +1,108 @@
 package org.jys.learn.leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author YueSong Jiang
  * @date 2020/4/24
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ *
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (35.11%)
+ * Likes:    2776
+ * Dislikes: 125
+ * Total Accepted:    439.4K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * Given an array of integers nums sorted in ascending order, find the starting
+ * and ending position of a given target value.
+ *
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ *
+ * If the target is not found in the array, return [-1, -1].
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ *
+ *
  */
 public class Lc0034FindFirstAndLastPositionOfElementInSortedArray {
+
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        int[] nums=new int[]{5,7,7,8,8,10};
+        System.out.println(Arrays.toString(solution.searchRange(nums, 8)));
+    }
+
+    static class Solution {
+        public int[] searchRange(int[] nums, int target) {
+
+            if(nums.length==0){
+                return new int[]{-1,-1};
+            }
+            //first binary search to find first equal value
+            int lo=0;
+            int hi=nums.length-1;
+            int mid=0;
+            while(lo<=hi){
+                mid=lo+((hi-lo)>>1);
+                if(nums[mid]==target){
+                    break;
+                }
+
+                if(nums[mid]>target){
+                    hi=mid-1;
+                }else{
+                    lo=mid+1;
+                }
+            }
+
+            //当前，mid/lo就是target或者找不到
+            if(nums[mid]!=target){
+                return new int[]{-1,-1};
+            }
+
+            //分别查找左右数组
+            //左半
+            lo=0;
+            hi=mid;
+            while(lo<hi){
+                mid=lo+((hi-lo)>>1);
+                if(nums[mid]<target){
+                    lo=mid+1;
+                }else{
+                    hi=mid;
+                }
+            }
+            int[] ret=new int[2];
+            ret[0]=lo;
+
+            //查找右一半
+            lo=mid;
+            hi=nums.length-1;
+            while(lo<hi){
+                mid=lo+((hi-lo)>>1)+1;
+                if(nums[mid]>target){
+                    hi=mid-1;
+                }else{
+                    lo=mid;
+                }
+            }
+            ret[1]=hi;
+            return ret;
+        }
+    }
 }
